0ba1845ec6faadd6637ab952f9bbc4e5c6bbb6e0
Joe Froula <jmfroula@gmail.com>
2022-09-02 08:21:37 -0700

    adds save btn

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ const form = document.getElementById('generate-form')
 const qr = document.getElementById('qrcode')
 
 const onGenerateSubmit = (e) => {
+
     e.preventDefault()
+
+    clearUi()
+
     const url = document.getElementById('url').value 
     const size = document.getElementById('size').value 
     console.log(size, url)
@@ -15,12 +19,16 @@ const onGenerateSubmit = (e) => {
     } else {
         
         showSpinner()
-        hideQr()
-        
+
         setTimeout(() => { 
-            showQr()
+            
             hideSpinner(); 
             generateCode(url, size);
+
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl)
+            }, 50)
 
         }, 1000)
 
@@ -35,19 +43,13 @@ const hideSpinner = () => {
     document.getElementById('spinner').style.display = 'none'
 }
 
-const showQr = () => {
-    qr.style.display = 'block'
+const clearUi = () => {
+    qr.innerHTML = ''
+    const saveLink = document.getElementById('save-link')
+    if(saveLink) { saveLink.remove() }
 }
-
-const hideQr = () => {
-    qr.style.display = 'none'
-}
-
-
 
 const generateCode = (url, size) => {
-    
-    qr.innerHTML = '' 
 
     const qrcode = new QRCode('qrcode', {
 
@@ -57,6 +59,17 @@ const generateCode = (url, size) => {
 
     })
     
+}
+
+const createSaveBtn = (saveUrl) => {
+
+    const link = document.createElement('a'); 
+    link.id = 'save-link'
+    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5'
+    link.href = saveUrl 
+    link.download = 'qrcode'
+    link.innerHTML = 'Save Image'
+    document.getElementById('generated').appendChild(link)
 }
 
 hideSpinner()
